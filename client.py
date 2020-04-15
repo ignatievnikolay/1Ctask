64e3d8d467eec7b6d2e0d9def713034f8fc433eb
ignatievnikolay <36414516+ignatievnikolay@users.noreply.github.com>
2020-04-15 23:23:00 +0300

    first version (not tested)

--- client.py
+++ client.py
@@ -47,24 +47,39 @@ def user_valid(sign_in_id: str):
     return False
 
 
+def add_user(name: str, surname: str):
+    user_id = str(uuid.uuid4())
+    c.execute('INSERT INTO User '
+              'VALUES (%s, %s, %s)' %
+              (user_id, name, surname))
+    return user_id
+
+
 def main():
     print('Write your user id')
     cur_user_id = -1
+    
+    print('sign_in or create user')
     while True:
-        sign_in_id = input()
-        if user_valid(sign_in_id):
-            cur_user_id = sign_in_id
-            break
+        cmd = input().split(' ')
+        if cmd[0] == 'sign_in':
+            if user_valid(sign_in_id=cmd[1]):
+                cur_user_id = cmd[1]
+                break
+        if cmd[0] == 'create':
+            new_user_id = add_user(name=cmd[1], surname=cmd[2])
+            print('your user id is:', new_user_id)
     if cur_user_id == -1:
         return 0
+    
     while True:
-        command = input().split(' ')
-        if command[0] == 'exit':
+        cmd = input().split(' ')
+        if cmd[0] == 'exit':
             break
-        if command[0] == 'recommend':
-            recommend(command[1], command[2], int(command[3]))
-        if command[0] == 'book':
-            book_room(command[1], command[2], command[3], cur_user_id, int(command[4]))
+        if cmd[0] == 'recommend':
+            recommend(cmd[1], cmd[2], int(cmd[3]))
+        if cmd[0] == 'book':
+            book_room(cmd[1], cmd[2], cmd[3], cur_user_id, int(cmd[4]))
 
 
 if __name__ == '__main__':
